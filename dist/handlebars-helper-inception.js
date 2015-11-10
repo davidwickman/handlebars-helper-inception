6ccf392c4b7e48106ce2a4577a3681b508c3be91
davidwickman <dave@wickmanstudios.com>
2015-11-10 15:55:01 -0500

    check for undefined value as to not error out if a value is undefined

--- dist/handlebars-helper-inception.js
+++ dist/handlebars-helper-inception.js
@@ -1,6 +1,6 @@
 // handlebars-helper-inception
 // -----------------
-// v0.1.1
+// v0.1.2
 //
 // http://github.com/davidwickman/handlebars-helper-inception
 
@@ -9,23 +9,23 @@ module.exports.register = function( Handlebars, options, params ) {
   Handlebars.registerHelper( 'helperInception', function( value ) {
 
     // Level 1: Reality
-    if ( value.indexOf( '{{' ) >= 0 ) {
+    if ( ( value !== undefined ) && ( value.indexOf( '{{' ) >= 0 ) ) {
       value = Handlebars.compile( value )( this );
 
       // Level 2: Van Chase
-      if ( value.indexOf( '{{' ) >= 0 ) {
+      if ( ( value !== undefined ) && ( value.indexOf( '{{' ) >= 0 ) ) {
         value = Handlebars.compile( value )( this );
 
         // Level 3: The Hotel
-        if ( value.indexOf( '{{' ) >= 0 ) {
+        if ( ( value !== undefined ) && ( value.indexOf( '{{' ) >= 0 ) ) {
           value = Handlebars.compile( value )( this );
 
           // Level 4: Snow Fortress
-          if ( value.indexOf( '{{' ) >= 0 ) {
+          if ( ( value !== undefined ) && ( value.indexOf( '{{' ) >= 0 ) ) {
             value = Handlebars.compile( value )( this );
 
             // Level 5: Limbo
-            if ( value.indexOf( '{{' ) >= 0 ) {
+            if ( ( value !== undefined ) && ( value.indexOf( '{{' ) >= 0 ) ) {
               value = Handlebars.compile( value )( this );
             }
           }
